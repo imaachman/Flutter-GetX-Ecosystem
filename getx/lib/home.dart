6384cf9b21c4ec2e69874300f28641b256097aa3
Aachman Garg <imaachman@gmail.com>
2024-05-24 13:12:42 +0530

    Add comments to getx project

--- getx/lib/home.dart
+++ getx/lib/home.dart
@@ -4,13 +4,18 @@ import 'package:get/get.dart';
 import 'controller.dart';
 import 'count_text.dart';
 
+// Home widget with a counter text and a floating action button to increment the
+// counter. It uses GetX to listen to the controller and update the UI.
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // GetX widget to listen to the controller notifications.
     return GetX<Controller>(
+      // Initialize the controller.
       init: Controller(),
+      // Builder to create the UI based on the controller state.
       builder: (controller) {
         return Scaffold(
           appBar: AppBar(
@@ -18,11 +23,13 @@ class Home extends StatelessWidget {
           ),
           body: Center(
             child: CountText(
+              // Get the latest count from the controller.
               count: controller.count.toString(),
               color: Colors.blue,
             ),
           ),
           floatingActionButton: FloatingActionButton(
+            // Increment the count when the button is pressed.
             onPressed: () => controller.incrementCount(),
             child: Icon(Icons.add),
           ),
