869009c076d31f50c5946aab90ae8308534b2f25
Aachman Garg <imaachman@gmail.com>
2024-05-24 13:32:54 +0530

    Add comments to Obx project

--- obx/lib/controller.dart
+++ obx/lib/controller.dart
@@ -1,7 +1,10 @@
 import 'package:get/get.dart';
 
+// Controller class that manages the counter value.
 class Controller extends GetxController {
+  // Counter value that can be observed.
   final RxInt count = 0.obs;
 
+  // Increments the counter value.
   void incrementCount() => count.value++;
 }
--- obx/lib/home.dart
+++ obx/lib/home.dart
@@ -4,9 +4,13 @@ import 'package:get/get.dart';
 import 'controller.dart';
 import 'count_text.dart';
 
+// Home widget with a counter and a button to increment the counter.
+// Counter value is updated using the Obx widget.
 class Home extends StatelessWidget {
   Home({super.key});
 
+  // Inject the controller using [Get.put] to make it available to the Obx
+  // widget.
   final Controller controller = Get.put(Controller());
 
   @override
@@ -16,14 +20,18 @@ class Home extends StatelessWidget {
         title: const Text('Obx Demo'),
       ),
       body: Center(
+        // Use the Obx widget to build and update the counter.
         child: Obx(
           () => CountText(
+            // Access the latest value of the counter using
+            // [controller.count.value].
             count: controller.count.value.toString(),
             color: Colors.blue,
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
+        // Increment the counter when the button is pressed.
         onPressed: () => controller.incrementCount(),
         child: const Icon(Icons.add),
       ),
