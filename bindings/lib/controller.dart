387e4b81ce5552bd9600c972ede98e1801daf041
Aachman Garg <imaachman@gmail.com>
2024-05-25 12:15:34 +0530

    Add comments to bindings project #2

--- bindings/lib/controller.dart
+++ bindings/lib/controller.dart
@@ -2,7 +2,9 @@ import 'package:get/get.dart';
 
 /// Controller class that manages the count.
 class Controller extends GetxController {
+  /// Reactive variable that stores the count.
   final RxInt count = 0.obs;
 
+  /// Method that increments the count.
   void incrementCount() => count.value++;
 }
