d5ffa12687145c5577936e7bb218ac7c50fadb8a
Aachman Garg <imaachman@gmail.com>
2024-05-31 18:45:21 +0530

    Fix import error in test

--- mixin_builder/test/widget_test.dart
+++ mixin_builder/test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:getx_controller/main.dart';
+import 'package:mixin_builder/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
